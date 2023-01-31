7213da3927398224adfb569d95eed638144e1beb
Daniyar <d.merekeyev@gmail.com>
2023-01-31 13:31:00 +0600

    Initial Commit

---
+++ Home_Kit/Home_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_KitApp.swift
+//  Home_Kit
+//
+//  Created by Daniyar Merekeyev on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Home_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
