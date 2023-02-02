38c11c04fd2a18ad809336af785065c054c1f26c
Daniyar <d.merekeyev@gmail.com>
2023-02-03 02:00:02 +0600

    Changed design

--- Home_Kit/MainView.swift
+++ Home_Kit/MainView.swift
@@ -11,19 +11,44 @@ struct MainView: View {
     @StateObject private var viewModel = MainViewModel()
     @State private var selectedIndex = 0
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.green]
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("Welcome to your smart home")
-                    .font(.system(size: 16))
-                    .frame(width: UIScreen.main.bounds.width * 0.915, alignment: .leading)
+                Text("Home")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(.systemGray3))
+                    .frame(width: UIScreen.main.bounds.width * 0.9, alignment: .leading)
+                HStack {
+                    Text("My Home")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 160)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "house.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 27))
+                        .padding(.trailing)
+                        .padding(.bottom, 7)
+                }
                 
-                ScrollView(.horizontal) {
+                Divider()
+                    .overlay(.white)
+                    .frame(width: UIScreen.main.bounds.width * 0.9)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
                         ForEach(viewModel.rooms.indices, id: \.self) { index in
                             RoomButtonView(icon: viewModel.rooms[index].icon, roomName: viewModel.rooms[index].roomName, isSelected: index == selectedIndex)
                                 .onTapGesture {
-                                    withAnimation() {
+                                    withAnimation(.easeOut) {
                                         selectedIndex = index
                                     }
                                 }
@@ -44,7 +69,10 @@ struct MainView: View {
                 
                 Spacer()
             }
-            .navigationTitle("Hi ALQ")
+            .background(content: {
+                Image("BackgroundImageThree")
+                    .ignoresSafeArea()
+            })
         }
     }
 }
--- Home_Kit/RoomButtonView.swift
+++ Home_Kit/RoomButtonView.swift
@@ -13,16 +13,38 @@ struct RoomButtonView: View {
     var isSelected: Bool
     
     var body: some View {
-        VStack {
-            Image(systemName: icon)
-                .foregroundColor(isSelected ? .white : .black)
-                .frame(width: 70, height: 70)
-                .background(isSelected ? Color("SelectedColor") : Color("CustomGray"))
-                .cornerRadius(15)
-            
-            Text(roomName)
-                .font(.system(size: 14))
+        if isSelected {
+            HStack {
+                Image(systemName: icon)
+                    .foregroundColor(.black)
+                    .font(.system(size: 16))
+                Text(roomName)
+                    .foregroundColor(.black)
+                    .font(.system(size: 16))
+                    .fontWeight(.semibold)
+            }
+            .frame(width: 140, height: 70)
+            .background(.white)
+            .cornerRadius(20)
+        } else {
+            HStack {
+                Image(systemName: icon)
+                    .foregroundColor(.white)
+                    .font(.system(size: 16))
+                Text(roomName)
+                    .foregroundColor(.white)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+            }
+            .frame(width: 140, height: 70)
+            .background(.ultraThinMaterial)
+            .cornerRadius(20)
         }
-        .frame(width: 80, height: 95)
+    }
+}
+
+struct RoomButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomButtonView(icon: "sofa", roomName: "Living room", isSelected: false)
     }
 }
